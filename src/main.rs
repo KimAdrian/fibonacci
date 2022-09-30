ae06df741be789ca22d5b3c802cb640f09cf923b
KimAdrian <adriankimutai5@gmail.com>
2022-09-30 13:21:44 +0300

    Basic implementation complete

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
 fn main() {
-    let mut number_of_terms = String::new();
-    let mut first_value = String::new();
+    // let mut number_of_terms = String::new();
+    // let mut first_value = String::new();
+    let mut a: i32 = 1;
+    let mut b: i32 = 1;
+    let n = 5;
+    let mut i = 1;
+
+    print!("{a} {b}");
+
+    while i <= n-2 {
+        let c = a + b;
+        print!(" {c}");
+        i += 1;
+
+        a = b;
+        b = c;
+    }
+
+
 }
