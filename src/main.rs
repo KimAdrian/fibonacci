275c11ae9517e50fcfa9c82941007faca6aa8a3b
KimAdrian <adriankimutai5@gmail.com>
2022-10-01 14:57:21 +0300

    Added comments and reformatted code with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,15 @@ fn main() {
 
     println!("Input number of terms in the sequence");
 
+    //Fetch user input
     io::stdin()
         .read_line(&mut number_of_terms)
         .expect("Failed to read line");
 
-
+    //Check to ensure input is a number
     let number_of_terms: u32 = match number_of_terms.trim().parse() {
-        Ok(value)=>value,
-        Err(_)=> {
+        Ok(value) => value,
+        Err(_) => {
             println!("Value should be a number");
             std::process::exit(1);
         }
@@ -21,10 +22,12 @@ fn main() {
 
     println!("Input starting value");
 
+    //Fetch user input
     io::stdin()
         .read_line(&mut first_value)
         .expect("Failed to read line");
 
+    //Check to ensure input is a number
     let mut first_value: u32 = match first_value.trim().parse() {
         Ok(num) => num,
         Err(_) => {
@@ -33,11 +36,12 @@ fn main() {
         }
     };
 
+    //Fibonacci algorithm
     let mut second_value = first_value;
 
     print!("{first_value}, {second_value}");
 
-    for mut _x in 1..=number_of_terms-2 {
+    for mut _x in 1..=number_of_terms - 2 {
         let c = first_value + second_value;
         print!(", {c}");
 
@@ -46,6 +50,4 @@ fn main() {
 
         _x += 1;
     }
-
-
 }
